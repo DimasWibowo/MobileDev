f92b5969d7659a347c8b6be1317a799c251efd7a
Nusa186 <kodratnusantara@gmail.com>
2023-09-06 20:09:08 +0700

    git commit -m "tambah binding di main"

--- app/src/main/java/com/example/mobileapps/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.mobileapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobileapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
     }
 }
